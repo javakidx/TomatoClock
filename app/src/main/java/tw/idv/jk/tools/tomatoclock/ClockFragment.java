95072342cbff9f268c90aaf3872bcf09e6dda9a1
javakidx <javakidx@gmail.com>
2015-09-03 13:06:16 +0800

    start work

--- app/src/main/java/tw/idv/jk/tools/tomatoclock/ClockFragment.java
+++ app/src/main/java/tw/idv/jk/tools/tomatoclock/ClockFragment.java
@@ -1,21 +1,41 @@
 package tw.idv.jk.tools.tomatoclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  *
  */
 public class ClockFragment extends Fragment
 {
+    private Button mStartWorkButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_clock,container, false);
 
-        return v;
+        return setView(v);
+    }
+
+    private View setView(final View view)
+    {
+        mStartWorkButton = (Button)view.findViewById(R.id.startClock_Button);
+        mStartWorkButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), ClockTimerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/tw/idv/jk/tools/tomatoclock/ClockTimerFragment.java
@@ -0,0 +1,21 @@
+package tw.idv.jk.tools.tomatoclock;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Javakid on 2015/9/3.
+ */
+public class ClockTimerFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_clock_timer,container, false);
+
+        return v;
+    }
+}
